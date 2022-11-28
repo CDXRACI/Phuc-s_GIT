5881b90f1e039023e9d5010da7da868a5ce9af43
CDXRACI <phuccd.icv@gmail.com>
2022-11-28 15:19:53 +0700

    Fixed STM32-SYSCLOCK

--- STM32-Tutorials/STX_CONFIGCLOCK/Core/Src/ST_CLOCKCONFIG.c
+++ STM32-Tutorials/STX_CONFIGCLOCK/Core/Src/ST_CLOCKCONFIG.c
@@ -7,9 +7,21 @@
 void SYS_CONFIG_CLOCK(void)
 {
 RCC->APB1ENR |= (1 << 28);
+RCC->APB2ENR |= (1 << 14);
+
 RCC->CR |= 1<<16;  
 
 while (!(RCC->CR & (1<<17)));
+
+#define PLL_M 	4
+#define PLL_N 	100
+#define PLL_P 	2  
+
+RCC->PLLCFGR |= (PLL_M <<0)   |\
+                (PLL_N << 6)  |\
+                (0<<16)       |\
+                (1<<17)       |\
+                (1<<22);
 
 
 RCC->CFGR &= ~(1<<4);
@@ -19,16 +31,15 @@ RCC->CFGR |= (4<<10);
 	
 
 RCC->CFGR &= ~(1<<13);
-#define PLL_M 	4
-#define PLL_N 	100
-#define PLL_P 	2  
 
-RCC->PLLCFGR = (PLL_M <<0) | (PLL_N << 6) | (PLL_P <<16) | (1<<22);
+
+
+
 RCC->CR |= (1<<24);
 while (!(RCC->CR & (1<<25)));
 RCC->CFGR |= (2<<0);
 while (!(RCC->CFGR & (2<<2)));
-RCC->APB2ENR |= (1<<14);
+
 
 
 }
